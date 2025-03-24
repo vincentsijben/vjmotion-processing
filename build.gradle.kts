269ca28ca7ef7606c07464e93734a8f1d22309d5
Vincent Sijben <vincent.sijben@zuyd.nl>
2025-03-24 10:40:07 +0100

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ group = "nl.genart"
 // - MINOR: Increases when you add new features that are backward-compatible.
 // - PATCH: Increases when you make backward-compatible bug fixes.
 // You can update these numbers as you release new versions of your library.
-version = "0.0.1"
+version = "0.0.2"
 
 // The location of your sketchbook folder. The sketchbook folder holds your installed
 // libraries, tools, and modes. It is needed if you:
